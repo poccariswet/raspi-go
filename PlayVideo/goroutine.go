58faebd48e49be5af4f5f234ba5ef7ded8580479
soeyusuke <s1240231@gmail.com>
2017-08-13 01:00:38 +0900

    [add] <試し>

--- PlayVideo/goroutine.go
+++ PlayVideo/goroutine.go
@@ -2,21 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
+var ok bool
+
 func main() {
 	r := raspi.NewAdaptor()
 	button := gpio.NewButtonDriver(r, "11")
+	ch := make(chan bool)
 
 	work := func() {
 		button.On(gpio.ButtonPush, func(data interface{}) {
 			fmt.Println("button pushed")
 
-      go func
+			if ok == true {
+				if <-ch == true {
+					out, err := exec.Command("killall", "omxplayer.bin").Output()
+					if err != nil {
+						log.Println(err)
+						os.Exit(100)
+					}
+					fmt.Println(string(out))
+				}
+			}
+
+			if ok != true {
+				go func(ch chan<- bool) {
+					ch <- true
+					ok = true
+					out, err := exec.Command("omxplayer", "/opt/vc/src/hello_pi/hello_video/test.h264").Output()
+					if err != nil {
+						log.Println(err)
+						os.Exit(100)
+					}
+					fmt.Println(string(out))
+				}(ch)
+			}
 		})
 	}
 
--- PlayVideo/test.go
+++ PlayVideo/test.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"time"
 )
 
 func main() {
 	q := make(chan bool)
+  music := "Music/iTunes/iTunes\ Media/Music/10-FEET/thread/03\ その向こうへ.m4a"
 
-	go func(q chan<- bool) {
-		time.Sleep(1 * time.Second)
+	go func(q chan<- bool) { // send
+		time.Sleep(3 * time.Second)
+		out, err := exec.Command("afplay", music ,"&").Output()
+		if err != nil {
+			log.Println(err)
+			os.Exit(100)
+		}
+		fmt.Println(string(out))
 		q <- true
 	}(q)
 
-	fmt.Println(<-q) // q に何か入るまで待つ
+	//fmt.Println(<-q) // q に何か入るまで待つ
+	if ok := <-q; ok == true {
+		out, err := exec.Command("killall", "afplay").Output()
+		if err != nil {
+			log.Println(err)
+			os.Exit(200)
+		}
+		fmt.Println(string(out))
+	}
 
 }
